48687928d05282e77c8940d9e15cd957ab15bd6f
Christoph Taucher <taucher.ch@gmail.com>
2018-11-12 15:59:31 +0100

    dev

--- pickups/Box.cs
+++ pickups/Box.cs
@@ -5,9 +5,11 @@ public class Box : StaticBody2D
 {
     private bool Taken = false;
     private AnimationPlayer AnimationPlayer;
+    private CollisionShape2D CollisionShape2D;
 
     public override void _Ready()
     {
+        CollisionShape2D = (CollisionShape2D)GetNode("CollisionShape2D");
         AnimationPlayer = (AnimationPlayer)GetNode("AnimationPlayer");
         AnimationPlayer.Play("idle");
     }
@@ -17,7 +19,8 @@ public class Box : StaticBody2D
         if (Taken)
             return false;
         Taken = true;
-        ((CollisionShape2D)GetNode("CollisionShape2D")).QueueFree();
+        CollisionShape2D.Disabled = true;
+        CollisionShape2D.QueueFree();
         AnimationPlayer.Play("taken");
         return true;
     }
