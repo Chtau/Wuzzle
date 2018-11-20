3756263e35a3ad3b038a02bb2b680ad588866d72
Christoph Taucher <taucher.ch@gmail.com>
2018-11-20 16:50:06 +0100

    dev

--- hud/Question.cs
+++ hud/Question.cs
@@ -11,9 +11,9 @@ public class Question : CanvasLayer
     private List<QuestionItem> questionQueue = new List<QuestionItem>();
 
     private Label question;
-    private Label answer1;
-    private Label answer2;
-    private Label answer3;
+    private Answer answer1;
+    private Answer answer2;
+    private Answer answer3;
     private Panel panel;
 
     public override void _Ready()
@@ -22,9 +22,9 @@ public class Question : CanvasLayer
         panel.Visible = false;
         InitQuestions();
         question = (Label)GetNode("/Panel/VBoxContainer/QuestionBox/QuestionLabel");
-        answer1 = (Label)GetNode("/Panel/VBoxContainer/HBoxContainer/Answer1");
-        answer2 = (Label)GetNode("/Panel/VBoxContainer/HBoxContainer2/Answer2");
-        answer3 = (Label)GetNode("/Panel/VBoxContainer/HBoxContainer3/Answer3");
+        answer1 = (Answer)GetNode("/Panel/VBoxContainer/AnswerBox");
+        answer2 = (Answer)GetNode("/Panel/VBoxContainer/AnswerBox2");
+        answer3 = (Answer)GetNode("/Panel/VBoxContainer/AnswerBox3");
     }
 
     public override void _Process(float delta)
@@ -33,15 +33,15 @@ public class Question : CanvasLayer
         {
             if (Input.IsActionPressed(GlobalValues.Keymap_Answer_1))
             {
-
+                HandleAnswer(answer1.IsCorrect);
             }
             else if (Input.IsActionPressed(GlobalValues.Keymap_Answer_2))
             {
-
+                HandleAnswer(answer2.IsCorrect);
             }
             else if (Input.IsActionPressed(GlobalValues.Keymap_Answer_3))
             {
-
+                HandleAnswer(answer3.IsCorrect);
             }
         }
     }
@@ -52,16 +52,43 @@ public class Question : CanvasLayer
 
         var item = questionQueue.First();
         question.Text = item.Question;
-        answer1.Text = item.Answer[0].Item1;
-        answer2.Text = item.Answer[1].Item1;
-        answer3.Text = item.Answer[2].Item1;
+        answer1.SetAnswer(item.Answer[0].Item1, item.Answer[0].Item2);
+        answer2.SetAnswer(item.Answer[1].Item1, item.Answer[1].Item2);
+        answer3.SetAnswer(item.Answer[2].Item1, item.Answer[2].Item2);
 
         panel.Visible = true;
+    }
+
+    private void HandleAnswer(bool result)
+    {
+        GD.Print("Answer result:" + result);
+        alreadyAnswered.Add(questionQueue.First().Id);
+        questions.RemoveAt(0);
+        questionQueue.RemoveAt(0);
+
+        if (questionQueue.Count > 0)
+        {
+            // show the next question
+            ShowQuestion();
+        } else
+        {
+            panel.Visible = false;
+        }
     }
 
     private void InitQuestions()
     {
         questions = new List<QuestionItem>();
+        questions.Add(new QuestionItem
+        {
+            Question = "Calculate the sum for '12 * 3 + 7'",
+            Answer = new List<Tuple<string, bool>>
+            {
+                new Tuple<string, bool>("43", true),
+                new Tuple<string, bool>("67", false),
+                new Tuple<string, bool>("50", false)
+            }
+        });
         alreadyAnswered = new List<Guid>();
     }
 
