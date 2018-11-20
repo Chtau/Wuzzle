153fcd357851e063bb9577d0cd1b9d740014afe8
Christoph Taucher <taucher.ch@gmail.com>
2018-11-20 16:37:55 +0100

    dev

---
+++ hud/Answer.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class Answer : HBoxContainer
+{
+    private Label answer;
+    private string text = null;
+
+    public bool IsCorrect { get; private set; }
+
+    public override void _Ready()
+    {
+        answer = (Label)GetNode("Answer");
+        if (!string.IsNullOrWhiteSpace(text))
+            answer.Text = text;
+    }
+
+    public void SetAnswer(string text, bool isCorrect)
+    {
+        this.text = text;
+        if (answer != null)
+        {
+            answer.Text = text;
+        }
+        IsCorrect = isCorrect;
+    }
+
+}
