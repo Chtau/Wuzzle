853de694536e965f7849fb9e5dcd3d76a0f278c1
Christoph Taucher <taucher.ch@gmail.com>
2018-11-24 17:21:02 +0100

    dev

---
+++ menu/StartMenu.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class StartMenu : Node
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+
+    public override void _Ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+        
+    }
+}
+
+
+
---
+++ menu/items/MenuButton.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+
+public class MenuButton : Button
+{
+    private string label = "Button";
+    [Export]
+    public string LabelText
+    {
+        get { return label; }
+        set
+        {
+            label = value;
+            OnLabelChange(label);
+        }
+    }
+
+    [Export]
+    public PackedScene Scene { get; set; }
+    
+    private Label labelNode;
+
+    public override void _Ready()
+    {
+        Connect("pressed", this, nameof(OnButtonPressed));
+
+        labelNode = (Label)GetNode("Label");
+        OnLabelChange(label);
+    }
+
+
+    private void OnLabelChange(string text)
+    {
+        if (labelNode != null)
+            labelNode.Text = text;
+    }
+
+    public void OnButtonPressed()
+    {
+        GD.Print("OnButtonPressed =>" + LabelText);
+        if (Scene != null)
+        {
+            GD.Print("Load Scene:" + Scene.ResourcePath);
+        } else
+        {
+            GD.Print("No Scene is set for the Button");
+        }
+    }
+}
