29e710e7c718b1164e5aa88327860347b7779282
Christoph Taucher <taucher.ch@gmail.com>
2018-11-30 13:49:24 +0100

    change Menu

--- menu/StartMenu.cs
+++ menu/StartMenu.cs
@@ -3,40 +3,77 @@ using System;
 
 public class StartMenu : Node
 {
-    private ScrollContainer optionsWrapper;
-    private LevelSelect levelSelect;
+    enum ContentType
+    {
+        None,
+        Levels,
+        Options,
+        Credits
+    }
+
+    private ScrollContainer options;
+    private LevelSelect levels;
+    private Panel contentPanel;
 
     public override void _Ready()
     {
-        optionsWrapper = (ScrollContainer)GetNode("MarginContainer/VBoxContainer/HBoxContainer/ScrollContainer");
-        levelSelect = (LevelSelect)GetNode("MarginContainer/VBoxContainer/HBoxContainer/LevelSelect");
+        contentPanel = (Panel)GetNode("ContainerContent/Panel");
+        levels = (LevelSelect)contentPanel.GetNode("MarginContainer/LevelSelect");
+        options = (ScrollContainer)contentPanel.GetNode("MarginContainer/ScrollContainer");
+        //optionsWrapper = (ScrollContainer)GetNode("MarginContainer/VBoxContainer/HBoxContainer/ScrollContainer");
+        //levelSelect = (LevelSelect)GetNode("MarginContainer/VBoxContainer/HBoxContainer/LevelSelect");
 
-        optionsWrapper.Visible = false;
-        levelSelect.SetVisible(true);
+        //optionsWrapper.Visible = false;
+        //levelSelect.SetVisible(true);
         //levelSelect.Visible = true;
-    }
 
-    private void _on_FadeIn_FadeInFinished(object foo, object bar)
-    {
-        // Replace with function body
+        OnShowContent(ContentType.None);
     }
 
     private void OnOptionsPressed()
     {
-        levelSelect.SetVisible(false);
-        optionsWrapper.Visible = true;
+        OnShowContent(ContentType.Options);
     }
 
-    private void OnStartPressed()
+    private void OnCreditsPressed()
     {
-        // Replace with function body
-        optionsWrapper.Visible = false;
-        levelSelect.SetVisible(true);
+        OnShowContent(ContentType.Credits);
+    }
+
+    private void OnLevelPressed()
+    {
+        OnShowContent(ContentType.Levels);
     }
 
     private void OnExitPressed()
     {
         // Replace with function body
         GetTree().Quit();
+    }
+
+    private void OnShowContent(ContentType contentType)
+    {
+        if (contentType == ContentType.Credits)
+        {
+            if (!contentPanel.IsVisible())
+                contentPanel.Visible = true;
+        } else if (contentType == ContentType.Options)
+        {
+            if (!contentPanel.IsVisible())
+                contentPanel.Visible = true;
+            levels.SetVisible(false);
+            options.Visible = true;
+        } else if (contentType == ContentType.Levels)
+        {
+            if (!contentPanel.IsVisible())
+                contentPanel.Visible = true;
+            levels.SetVisible(true);
+            options.Visible = false;
+        } else
+        {
+            contentPanel.Visible = false;
+            levels.SetVisible(false);
+            options.Visible = false;
+        }
     }
 }
