0447a148af4f71864fda7635344442a904c5d529
Christoph Taucher <taucher.ch@gmail.com>
2018-12-02 20:51:05 +0100

    change level Finished message

--- hud/LevelFinishedMessage.cs
+++ hud/LevelFinishedMessage.cs
@@ -7,21 +7,33 @@ public class LevelFinishedMessage : Control
     private Label time;
     private Label previousRecord;
     private Label newRecord;
-    private Panel container;
+    private NinePatchRect container;
     private Button nextLevel;
+    private Button back;
     private AnimationPlayer animationPlayer;
+    private TextureRect goldRecord;
+    private TextureRect silberRecord;
+    private TextureRect bronzeRecord;
 
     private LevelItem nextLevelItem;
+    private LevelItem currentLevelItem;
 
     public override void _Ready()
     {
-        container = (Panel)GetNode("CanvasLayer/Panel");
+        container = (NinePatchRect)GetNode("CanvasLayer/Panel");
         time = (Label)container.GetNode("CenterContainer/VBoxContainer/TimeWrapper/Value");
         previousRecord = (Label)container.GetNode("CenterContainer/VBoxContainer/PreviousRecordWrapper/Value");
         newRecord = (Label)container.GetNode("CenterContainer/VBoxContainer/NewRecordWrapper/Label");
-        nextLevel = (Button)container.GetNode("CenterContainer/VBoxContainer/NextLevelWrapper/Button");
+        goldRecord = (TextureRect)container.GetNode("CenterContainer/VBoxContainer/NewRecordWrapper/GoldRecord");
+        silberRecord = (TextureRect)container.GetNode("CenterContainer/VBoxContainer/NewRecordWrapper/SilberRecord");
+        bronzeRecord = (TextureRect)container.GetNode("CenterContainer/VBoxContainer/NewRecordWrapper/BronzeRecord");
+        nextLevel = (Button)container.GetNode("CenterContainer/VBoxContainer/NextLevelWrapper/NextLevel");
+        back = (Button)container.GetNode("CenterContainer/VBoxContainer/NextLevelWrapper/BackToMenu");
         animationPlayer = (AnimationPlayer)GetNode("AnimationPlayer");
 
+        goldRecord.Visible = false;
+        silberRecord.Visible = false;
+        bronzeRecord.Visible = false;
         this.Visible = false;
         nextLevelItem = null;
         newRecord.Visible = false;
@@ -30,23 +42,31 @@ public class LevelFinishedMessage : Control
 
     private void OnButtonPressed()
     {
-        // go to next level
-        if (nextLevelItem != null)
-        {
-            SharedFunctions.Instance.LoadScene(this, nextLevelItem.ScenePath);
-        } else
-        {
-            SharedFunctions.Instance.LoadScene(this, GlobalValues.MainMenuScene);
-        }
+        SharedFunctions.Instance.LoadScene(this, nextLevelItem.ScenePath);
+    }
+
+    private void OnBackPressed()
+    {
+        SharedFunctions.Instance.LoadScene(this, GlobalValues.MainMenuScene);
+    }
+
+    private void OnRetryPressed()
+    {
+        SharedFunctions.Instance.LoadScene(this, currentLevelItem.ScenePath);
     }
 
     public void Show(LevelItem levelValues, TimeSpan timeSpan)
     {
+        currentLevelItem = levelValues;
         nextLevelItem = SharedFunctions.Instance.LevelManager.Next(levelValues.Order);
         if (nextLevelItem == null)
-            nextLevel.Text = "Back to Menu";
+        {
+            nextLevel.Visible = false;
+        }
         else
-            nextLevel.Text = "Next Level";
+        {
+            nextLevel.Visible = true;
+        }
 
         time.Text = SharedFunctions.Instance.FormatTimeSpan(timeSpan);
 
@@ -54,6 +74,16 @@ public class LevelFinishedMessage : Control
         {
             newRecord.Visible = true;
             animationPlayer.Play("new_record", -1, 5);
+            if (timeSpan <= levelValues.GoldTime)
+            {
+                goldRecord.Visible = true;
+            } else if (timeSpan <= levelValues.SilverTime)
+            {
+                silberRecord.Visible = true;
+            } else if (timeSpan <= levelValues.BronzeTime)
+            {
+                bronzeRecord.Visible = true;
+            }
         }
         if (levelValues.Record.HasValue)
             previousRecord.Text = SharedFunctions.Instance.FormatTimeSpan(levelValues.Record.Value);
